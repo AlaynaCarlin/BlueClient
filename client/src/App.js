5dd0e6028bf217b3ae78485188fea10d866a42f4
reneegodby <57304584+reneegodby@users.noreply.github.com>
2021-12-02 21:21:35 -0500

    created login/navbar/signup/auth/footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import LogIn from './Components/login';
-import SignUp from './Components/signup';
+import Login from './Components/auth/Login';
+import SignUp from './Components/auth/Signup';
+import Footer from './Components/Site/Footer';
+import Auth from './Components/auth/Auth';
+import Sitebar from './Components/auth/Navbar'
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+
+  }
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('')
+  }
+
+  const protectedViews = () => {
+    // return (sessionToken === localStorage.getItem('token') ? <LogIndex token={sessionToken}/>
+    // : <Auth updateToken={updateToken}/>)
+  }
+
+
   return (
     <div className="App">
-      <SignUp />
-     <LogIn />
+      
+     <Auth />
+     {/* <Sitebar clickLogout={clearToken} /> */}
+      {/* {protectedViews()} */}
+      <Sitebar />
+     <SignUp />
+     <Login />
+     <Footer />
     </div>
   );
 }

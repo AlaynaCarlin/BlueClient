64dc4b3c983aae48db87294df46182094d04eed3
AlaynaCarlin <alaynacarlin18@gmail.com>
2021-12-02 21:07:28 -0500

    trying to work on get

---
+++ client/src/Components/logs/LogIndex.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react";
+import {Container, Row, Col} from 'reactstrap';
+import GetLogs from './GetLogs';
+import { useEffect, useState } from 'react';
+
+const LogIndex = (props) => {
+    const [workouts, setWorkouts] = useState([]);
+
+    const fetchLogs = () => {
+        fetch('http://localhost:3000/log/mine', {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${props.token}`
+            })
+        }) .then ((res) => res.json())
+            .then ((logData) => {
+                setLogs(logData);
+                console.log(logData);
+            })
+    }
+}

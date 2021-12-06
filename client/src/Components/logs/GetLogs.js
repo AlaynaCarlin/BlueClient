6fd383b8308e08f2c07560c938e02c216c1c12f4
reneegodby <57304584+reneegodby@users.noreply.github.com>
2021-12-06 16:16:34 -0500

    got specific today dates to display

--- client/src/Components/logs/GetLogs.js
+++ client/src/Components/logs/GetLogs.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { Table, Button } from "reactstrap";
 
 const LogTable = (props) => {
-  // console.log(props.foodLogs[0].date)
-  const logMapper = () => {
-    return props.foodLogs?.map((log, index) => {
+  const [isSpecific, setIsSpecific] = useState(false)
+  const [specificDateArr, setSpecificDateArr] = useState([])    //global variable
+
+
+  const logMapper = () => {         //mapping over all the logs
+    let logs = isSpecific ? specificDateArr : props.foodLogs //isSpecific by default is false and we need to check if it's true or false so we know what array to map over. If true, map over specificDateArr. If false, map over entire foodLog array
+    return logs.map((log, index) => {       //logs is a variable variable. Checks btwn sets of data
       return (
         <tr key={index}>
           <th scope="row">{log.id}</th>
@@ -20,35 +24,34 @@ const LogTable = (props) => {
     });
   };
 
-  const dayLogs = () => {
-    const dateToday = new Date(Date.now()).toISOString().slice(0, 10); //correctly formatted date
-    let date;
+  const findCurrentDay = () => {        //compares dates and displays same dates
+    const dateToday = new Date(Date.now()).toISOString().slice(0, 10);
+    // console.log(dateToday)
+    setSpecificDateArr(props.foodLogs.filter(log => {       //filtered array = global array
+        return log.date.slice(0,10) === dateToday       //filtered array is comparing all dates to today.
+    })); 
+    setIsSpecific(!isSpecific)  //toggle to clear
+  }
 
-    props.foodLogs.map((log, index) => {
-      console.log(log.date);
-      date = log.date.slice(0, 10);
-      if (date === dateToday) {
-        return (
-          <tr key={index}>
-            <th scope="row">{log.id}</th>
-            <td>{log.what}</td>
-            <td>{log.where}</td>
-            <td>{log.calories}</td>
-            <td>{log.category}</td>
-            <td>{dateToday}</td>
-            <td>{log.photo}</td>
-            <td>{log.feelings}</td>
-          </tr>
-        );
-      } else {
-        console.log("there was an error");
-      }
+  const findLastWeek = () => {
+    const dateToday = new Date(Date.now()).toISOString().slice(0, 10);
+    const lastWeek = props.foodLogs.filter(log => {
+        return log.date.slice(0, 10) ===  (dateToday);
     });
-  };
+    // lastWeek.setDate(lastWeek.getDate() - 7);
+    setSpecificDateArr(lastWeek)
+    setIsSpecific(!isSpecific)
+  }
+
+
 
   return (
     <>
       <h3>Log History</h3>
+      <Button onClick={findCurrentDay}>Current Day</Button>
+      <Button onClick={findLastWeek}>View Last Week</Button>
+      <Button>View Last 30 Days</Button>
+      
       <tbody></tbody>
       <hr />
       <Table striped>
@@ -65,10 +68,11 @@ const LogTable = (props) => {
           </tr>
         </thead>
         <tbody>
-          {dayLogs()}
           {logMapper()}
         </tbody>
+        
       </Table>
+      
     </>
   );
 };

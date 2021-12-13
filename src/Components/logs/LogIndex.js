f881e313340c949358777b7b2eb56689cd25db8b
AlaynaCarlin <alaynacarlin18@gmail.com>
2021-12-13 11:17:03 -0500

    styled logs on log page

--- src/Components/logs/LogIndex.js
+++ src/Components/logs/LogIndex.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import {scroll} from 'react-scroll';
 import GetLogs from './GetLogs';
 import LogFood from './CreateLog';
 import UpdateFood from './UpdateLog';
+import Footer from '../Site/Footer';
 
 const LogIndex = (props) => {
     const [foodLogs, setFoodLogs] = useState([]);
@@ -38,6 +40,10 @@ const LogIndex = (props) => {
         fetchLogs();
     }, [])
 
+    const scrollMore = () => {
+        scroll.scrollMore(100);
+    }
+
 console.log(foodLogs)
     return (
 
@@ -46,13 +52,14 @@ console.log(foodLogs)
                     <LogFood fetchLogs={fetchLogs} token={props.token}></LogFood>
                 </Row>
                 <Row className='getLogs'>
-                    {foodLogs !== {} ? <GetLogs  foodLogs={foodLogs} fetchLogs={fetchLogs} editUpdateLog={editUpdateLog} updateOn={updateOn} token={props.token} /> : ""}
+                    {foodLogs !== {} ? <GetLogs scrollMore={scrollMore}  foodLogs={foodLogs} fetchLogs={fetchLogs} editUpdateLog={editUpdateLog} updateOn={updateOn} token={props.token} /> : ""}
                     {/*  */}
                 </Row>
                 <Col>
                 {updateActive ? <UpdateFood logToUpdate={logToUpdate}
                 updateOff={updateOff} token={props.token} fetchLogs={fetchLogs}/> : <></>} 
                 </Col>
+                {/* <Footer className="loginStyle" /> */}
         </Container>
     )
 }
